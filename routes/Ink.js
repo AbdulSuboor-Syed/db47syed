38cba524ee6e60961eae01448abc65d53fc3404c
Syed, Abdul Suboor <77645775+AbdulSuboor-Syed@users.noreply.github.com>
2021-04-07 18:42:16 -0500

    added ink router

--- routes/Ink.js
+++ routes/Ink.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ink', { title: 'Sreach Result Ink' });
+  res.render('ink', { title: 'Sreach Result ink' });
 });
 
 module.exports = router;
